9f1779b280db1b445ce1de7ed8260f65cdb6597c
yahir <contrerasyahir18@gmail.com>
2024-10-10 13:43:05 -0600

    corrige cosas

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+'''
+1. Programa que lea un nombre y muestre en pantalla un saludo
+Entradas:
+   nombre: string
+Salidas:
+   saludo: string
+'''
+nombre = input("Escribe tu nombre")
+saludo = "hola " + nombre + " XD"
+print(saludo)
